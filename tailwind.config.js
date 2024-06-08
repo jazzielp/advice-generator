f4d28d53f342f366cfcb122a1dc9300b8b18cd47
Jazziel Puente <60794855+jazzielp@users.noreply.github.com>
2024-06-08 12:43:19 -0700

    💄 Add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,18 @@ export default {
     './src/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
+    theme: {
+      fontFamily: {
+        manrope: ['Manrope', 'sans-serif']
+      },
+      colors: {
+        'dark-blue': '#202733',
+        'dark-grayish-blue': '#313A48',
+        'grayish-blue': '#4F5D74',
+        'light-cyan': '#CEE3E9',
+        'neon-green': '#53FFAA'
+      }
+    },
     extend: {}
   },
   plugins: []
