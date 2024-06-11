1375b0dc3a7dfedc68b1444c62d3d6c5ddd43dff
Jazziel Puente <60794855+jazzielp@users.noreply.github.com>
2024-06-11 10:34:01 -0700

    💄 Style the aplication

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
+import Dice from './assets/Icons/Dice'
+import PatternDividerDesktop from './assets/Icons/PatternDividerDesktop'
+
 function App () {
   return (
-    <>
-      <h1 className='text-red-400'>Vite + React</h1>
-    </>
+    <div className='bg-dark-grayish-blue rounded-2xl p-12 max-w-[540px] text-center mx-auto relative'>
+      <h1 className='font-manrope text-[13px] font-extrabold text-red-400 text-neon-green tracking-[4.9px] mb-6'>ADVICE #177</h1>
+      <p className='font-manrope font-extrabold text-[28px] tracking-[-0.3px] text-light-cyan mb-10'>“It is easy to sit up and take notice, what's difficult is getting up and taking action.”</p>
+      <PatternDividerDesktop className='mb-6' />
+      <button className='flex justify-center items-center size-16 rounded-full bg-neon-green absolute bottom-[-25px] left-[50%] transform -translate-x-1/2 cursor-pointer hover:shadow-neon-green transition-all duration-200 ease-in-out'>
+        <Dice />
+      </button>
+    </div>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,19 +5,21 @@ export default {
     './src/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
-    theme: {
-      fontFamily: {
-        manrope: ['Manrope', 'sans-serif']
-      },
-      colors: {
-        'dark-blue': '#202733',
-        'dark-grayish-blue': '#313A48',
-        'grayish-blue': '#4F5D74',
-        'light-cyan': '#CEE3E9',
-        'neon-green': '#53FFAA'
-      }
+    fontFamily: {
+      manrope: ['Manrope', 'sans-serif']
     },
-    extend: {}
+    colors: {
+      'dark-blue': '#202733',
+      'dark-grayish-blue': '#313A48',
+      'grayish-blue': '#4F5D74',
+      'light-cyan': '#CEE3E9',
+      'neon-green': '#53FFAA'
+    },
+    extend: {
+      boxShadow: {
+        'neon-green': '0px 4px 30px -11px rgba(83,255,170,1)'
+      }
+    }
   },
   plugins: []
 }
