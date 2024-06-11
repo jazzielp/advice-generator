43b849d63e75abfd80ab02ef1682280263171b68
Jazziel Puente <60794855+jazzielp@users.noreply.github.com>
2024-06-11 11:37:03 -0700

    ♻️ Remove the useDeviceType hook as it is not needed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
 import Dice from './assets/Icons/Dice'
 import { PatternDividerDesktop } from './assets/Icons/PatternDividerDesktop'
 import { PatternDividerMobile } from './assets/Icons/PatternDividerMobile'
-import useDeviceType from './hooks/useDeviceType'
 
 function App () {
-  const { deviceType } = useDeviceType()
-  console.log(deviceType)
   return (
     <div className='bg-dark-grayish-blue rounded-2xl px-6 md:px-12 py-12 w-[343px] md:w-[540px] text-center mx-auto relative'>
       <h1 className='font-manrope text-[11px] md:text-[13px] font-extrabold text-red-400 text-neon-green tracking-[4.9px] mb-6'>ADVICE #177</h1>
